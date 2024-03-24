1b01fde818ae7235aa4f7e8237b7bd26d2ba0e22
kym <melvinkimani81@gmail.com>
2024-03-24 22:13:55 +0300

    creating myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names){
+    let thankYouMessages = [];
+    for(let i = 0; i < names.length; i++){
+        thankYouMessages.push(`Thank You, ${names[i]}!`);
+    }
+    return thankYouMessages;
+}
+function countdown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+
+
